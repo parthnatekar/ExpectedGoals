d6b63087b2c016bcb3dc2a429ab50ec6b73f002d
Parth Natekar <patnat26@gmail.com>
2022-11-25 23:25:20 -0800

    Added linear model

--- build_model.py
+++ build_model.py
@@ -1,11 +1,17 @@
 import numpy as np
 import pandas as pd
 from scrape_data import *
+from sklearn.linear_model import LinearRegression
 
-class LinearModel:
+class DataGenerator:
 
-	def __init__(self, dataframe):
-		self.data = dataframe
+	def __init__(self, dataframe1, dataframe2=None):
+		self.data = dataframe1
+		self.data = self.data.add_prefix('club_')
+
+		if dataframe2 is not None:
+			self.data2 = dataframe2
+			self.data2 = self.data2.add_prefix('international_')
 
 	def getStatistics(self, column_name = ''):
 		column = self.data[column_name].values
@@ -13,29 +19,63 @@ class LinearModel:
 		variance = np.var(column)
 		return mean, variance
 
-	def buildLinearModel(self, variables_dict, model_type = 'linear_regression'):
-		pass
-
 	def createTeamVariable(self, column_name):
 		group_data = {}
-		for team in self.data.groupby(by = ['team']).groups.keys():
-			group_data[team] = self.data.groupby(by = ['team']).get_group(team)
+		for team in self.data.groupby(by = ['club_team']).groups.keys():
+			group_data[team] = self.data.groupby(by = ['club_team']).get_group(team)
 
 		group_statistic = []
 		for i in range(len(self.data)):
-			player_team = self.data.iloc[i]['team']
-			team_data_without_player = group_data[player_team][group_data[player_team]['player'] != self.data.iloc[i]['player']]
-			group_statistic.append(np.mean(team_data_without_player[column_name].values))
+			player_team = self.data.iloc[i]['club_team']
+			team_data_without_player = group_data[player_team][group_data[player_team]['club_player'] != self.data.iloc[i]['club_player']]
+			group_statistic.append(np.mean(team_data_without_player['club_' + column_name].values))
 
-		self.data['rest_of_team_{}'.format(column_name)] = pd.Series(group_statistic)
+		self.data['rest_of_team_{}'.format('club_' + column_name)] = pd.Series(group_statistic)
 
-		# print(self.data.groupby(by = ['team']).get_group('Manchester City'))
+		group_data = {}
+		for team in self.data2.groupby(by = ['international_team']).groups.keys():
+			group_data[team] = self.data2.groupby(by = ['international_team']).get_group(team)
 
-		
+		group_statistic = []
+		for i in range(len(self.data2)):
+			player_team = self.data2.iloc[i]['international_team']
+			team_data_without_player = group_data[player_team][group_data[player_team]['international_player'] != self.data2.iloc[i]['international_player']]
+			group_statistic.append(np.mean(team_data_without_player['international_' + column_name].values))
+
+		self.data2['rest_of_team_{}'.format('international_' + column_name)] = pd.Series(group_statistic)
+
+	def createCombinedDataframe(self):
+		return pd.merge(self.data, self.data2, left_on = 'club_player', right_on='international_player')
+
+class LinearModel:
+
+	def __init__(self, dataframe):
+		self.data = dataframe
+
+	def buildLinearModel(self, variables_dict, model_type = 'linear_regression'):
+		y = self.data[variables_dict['dependent']]
+		X = self.data[variables_dict['independent']]
+
+		reg = LinearRegression().fit(X, y)
+
+		print(reg.score(X, y), reg.coef_)
 
 if __name__ == '__main__':
-	dataframe = pd.read_csv('/home/parth/Projects/UCSD/ProbabilityAndStatistics/Project/PL2021_Outfield.csv')
-	# variables_dict = {'dependent': 'goals_per90','independent_player': ''}
-	L = LinearModel(dataframe)
+	dataframe = pd.read_csv('/home/parth/Projects/UCSD/ProbabilityAndStatistics/Project/data/PL2020-21_Outfield.csv')
+	dataframe2 = pd.read_csv('/home/parth/Projects/UCSD/ProbabilityAndStatistics/Project/data/Euro2021_Outfield.csv')
+	L = DataGenerator(dataframe, dataframe2)
 
-	L.createTeamVariable('assists')
+	L.createTeamVariable('assists_per90')
+	L.createTeamVariable('goals_per90')
+	L.createTeamVariable('xg_xg_assist_per90')
+	L.createTeamVariable('passes_pct')
+	L.createTeamVariable('passes_completed')
+
+	merged = L.createCombinedDataframe()
+	merged.to_csv('Euro21_PL21.csv')
+
+	variables_dict = {'dependent': 'international_goals_per90',
+	'independent': ['club_goals_per90', 'rest_of_team_international_assists_per90', 'rest_of_team_international_xg_xg_assist_per90']}
+
+	M = LinearModel(merged)
+	M.buildLinearModel(variables_dict)
--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 from statsbombpy import sb
 from scrape_data import *
 
-df_outfield = get_outfield_data('https://fbref.com/en/comps/9/','/Premier-League-Stats')
+df_outfield = get_outfield_data('https://fbref.com/en/comps/9/2018-2019/','/2018-2019-Premier-League-Stats')
 
-df_outfield.to_csv('PL2021_Outfield.csv',index=False)
+df_outfield.to_csv('data/PL2018-19_Outfield.csv',index=False)
 
-df_outfield_WC = get_outfield_data('https://fbref.com/en/comps/1/2018/','/2018-FIFA-World-Cup-Stats')
+# df_outfield_WC = get_outfield_data('https://fbref.com/en/comps/676/','/European-Championship-Stats')
 
 
-df_outfield_WC.to_csv('WC2018_Outfield.csv',index=False)
+# df_outfield_WC.to_csv('Euro2021_Outfield.csv',index=False)
 
 # For Goalkeepers, row names have changed, need to manually replace those before
 # extracting
 
-df_keeper = get_keeper_data('https://fbref.com/en/comps/9/','/Premier-League-Stats')
+# df_keeper = get_keeper_data('https://fbref.com/en/comps/9/','/Premier-League-Stats')
 
-df_keeper.to_csv('PL2021_keepers.csv',index=False)
+# df_keeper.to_csv('PL2021_keepers.csv',index=False)
